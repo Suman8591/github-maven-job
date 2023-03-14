b7ab4b184ad0e8ed92d7f5995e5d1d795accbd6d
Suman8591 <125398534+Suman8591@users.noreply.github.com>
2023-03-14 16:31:24 +0530

    poll scm

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " CI maven Github job ");
+        System.out.println( " poll scm trigger ");
     }
 }
